64fd2c6a4cc15c64becfd9b3baf6994dbe33e30f
Aben <126703861+abengl@users.noreply.github.com>
2024-03-19 21:36:24 -0500

    Hito 2

--- src/main.js
+++ src/main.js
@@ -1,36 +1,59 @@
-// arreglo   que contiene las repsuestas correctas
-let correctas= [1,1,2];
-
-//arreglo donde se guarda las repsuestas del usuario
-let opcion_elegida=[];
-
-let cantidad_correctas=0;
-
-//funcion que toma el num de pregunta y el input elegido de esa pregunta
-function respuesta(num_pregunta, seleccionada){
-  //guardo la respuesta elegida
-  opcion_elegida[num_pregunta]= seleccionada.value;
-  //color blanco el fondo de las preguntas que no amrcas
-  //armo el id para seleccionar el section correspondiente
-  id="p" +num_pregunta;
+/*Esta es la función saludo, toma el nombre de la usuaria del input con el método getElementById, 
+lo almacena en una variable y la retorna anclado en el elemento párrafo en el archivo index*/
+//Se usa la declaración de una función como método para crear la función por ser la más intuitiva 
+//y sencilla para este caso
+function saludo() {
+  //Uso de const para definir la variable porque esta asumirá un solo valor no modificable
+  //Se utilizan los selectores getElementById para acceder a los valores de los elementos del DOM
+  //Accede al input by id username y luego captura su valor, que es el nombre de la usuaria
+  const username = document.getElementById("username").value; 
+  //Accede al elemento parrafo para asignarle a una variable
+  const saludo = document.getElementById("saludo");
   
-  labels= document.getElementById(id).childNotes;
-  labels[3].style.backgroundColor = "white";
-  labels[5].style.backgroundColor = "white";
-  labels[7].style.backgroundColor = "white";
-  
-  //doy el color a label seleccionado
-  seleccionada.parentNode.style.backgroundColor="#cec0fc"
+  //Inserta el nombre de la usuaria en el elemento parrafo
+  saludo.innerHTML = `Hola ${username}! Bienvenida a Trivias K-Drama`;
 }
 
-//funcion que compara los arreglos para saber cuantas estuvieron correctas
-function corregir(){
-  cantidad correctas=0;
-  for(i=0: i< correctas.length;i++){
-    if(correctas[i]==opcion_elegida[i]){
-      cantidad_correctas++;
-    }
+/*--------------------------------------------------------------------*/
+
+/*En esta sección se desarrolla la función para validar las respuestas correctas*/
+
+//Arreglo que contiene las repsuestas correctas
+const respuestasCorrectas = ["1", "1", "3"];
+
+//Obtenemos el elemento formulario para luego sacar las respuestas de la usuaria
+const form = document.getElementById("form");
+
+//Creamos una función para obtener las respuestas de la usuaria y comparar sus resultados
+//La función toma el parametro event ya que el boton de tipo submit genera un evento
+function validarRespuestas(event) {
+  //Prevenimos que el navegador aplique acciones default luego del envío del formulario,
+  //en este caso, lo normal sería que refresque la página, pero no es lo que queremos
+  //ya que seguiremos con otro proceso. Esto es un estandard
+  event.preventDefault();
+
+  //Accedemos a las respuestas de la usuaria del formulario
+  //Se usa queryselector para acceder elementos en base a sus atributos no únicos
+  const p1Respuesta = document.querySelector('input[name="p1"]:checked').value;
+  const p2Respuesta = document.querySelector('input[name="p2"]:checked').value;
+  const p3Respuesta = document.querySelector('input[name="p3"]:checked').value;
+
+  //variable para contar las respuestas correctas
+  let countRespuestasCorrectas = 0;
+
+  //Comparamos las respuesta de la usuaria con las respuestas correctas
+  if (p1Respuesta === respuestasCorrectas[0]) {
+    countRespuestasCorrectas++;
   }
-  
-  document.getElementById("resultado").innerHTML = cantidad_correctas;
+  if (p2Respuesta === respuestasCorrectas[1]) {
+    countRespuestasCorrectas++;
+  }
+  if (p3Respuesta === respuestasCorrectas[2]) {
+    countRespuestasCorrectas++;
+  }
+
+  //Creamos una variables que bote los aciertos del conteo
+  const aciertosConteo = document.getElementById("conteo");
+
+  aciertosConteo.innerHTML = `Puntaje final: ${countRespuestasCorrectas}`;
 }
