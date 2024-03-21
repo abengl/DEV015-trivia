610a492824e05b0e6c6d5e665d4e77dfb9d091d9
Aben <126703861+abengl@users.noreply.github.com>
2024-03-21 14:44:35 -0500

    Hito 4 v2

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function validarNombre() {
   // Si el nombre no está vacío, se almacenará en la memoria local y se redirigirá a la página Trivia.html
   if (username !== "") {
     localStorage.setItem("username", username);
-    window.location.href = "/Trivia/trivia.html";
+    window.location.href = "Trivia/trivia.html";
   } else {
     // Si el nombre está vacío, se mostrará una alerta pidiendo que se ingrese el nombre
     alert("¡Por favor ingresa tu nombre para iniciar la trivia!");
